41008634fb290fd10def3cf336908423cc585c84
Fillll <spam@fillll.ru>
2013-06-18 23:09:10 +0400

    RE: перезд на wiringpi

--- hardware/rotary/RotatyEncoder.py
+++ hardware/rotary/RotatyEncoder.py
@@ -1,74 +1,115 @@
-# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------
+# rotary_encoder.py from https://github.com/guyc/py-gaugette
+# Guy Carpenter, Clearwater Software
+#
+# This is a class for reading quadrature rotary encoders
+# like the PEC11 Series available from Adafruit:
+#   http://www.adafruit.com/products/377
+# The datasheet for this encoder is here:
+#   http://www.adafruit.com/datasheets/pec11.pdf
+#
+# This library expects the common pin C to be connected
+# to ground.  Pins A and B will have their pull-up resistor
+# pulled high.
+# 
+# Usage:
+#
+#     import gaugette.rotary_encoder
+#     A_PIN = 7  # use wiring pin numbers here
+#     B_PIN = 9
+#     encoder = gaugette.rotary_encoder.RotaryEncoder(A_PIN, B_PIN)
+#     while 1:
+#       delta = encoder.delta() # returns 0,1,or -1
+#       if delta!=0:
+#         print delta
 
-import RPi.GPIO as GPIO
+import wiringpi
+import math
+import threading
 import time
 
-class RotaryEnc(object):
-    '''
-    Using rotary encoder.
-    '''
-    
-    def __init__(self, PinA, PinB, PinButton):
-        self.PinA = PinA
-        self.PinB = PinB
-        self.PinButton = PinButton
-        self.old_button = 1
-        self.oldPinA = 1
-        self.button_release = 0
-        self.button_down = 0
-        
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.PinA, GPIO.IN)
-        GPIO.setup(self.PinB, GPIO.IN)
-        GPIO.setup(self.PinButton, GPIO.IN)
+class RotaryEncoder:
 
-        self.r_r = read_rot()
+    #----------------------------------------------------------------------
+    # Pass the wiring pin numbers here.  See:
+    #  https://projects.drogon.net/raspberry-pi/wiringpi/pins/
+    #----------------------------------------------------------------------
+    def __init__(self, a_pin, b_pin):
+        self.a_pin = a_pin
+        self.b_pin = b_pin
 
-    def bin_worker(self):
-        interval = 0
-        nothing_happens = 0
-        while (True):
-            a = self.get_delta()
-            if not a == 0:
-                print a
-                interval = 0
-                nothing_happens = 0
-            else:
-                nothing_happens += 1
-            
-            if   nothing_happens == 1000:
-                interval = 0.01
-            elif nothing_happens == 100000:
-                interval = 0.1
-                
-            time.sleep(interval)
-    
-    def read_rot(self):
-        # a_state = self.gpio.digitalRead(self.a_pin)
-        # b_state = self.gpio.digitalRead(self.b_pin)
-        # r_seq = (a_state ^ b_state) | b_state << 1
-        # return r_seq
-        a_state = GPIO.input(self.PinA)
-        b_state = GPIO.input(self.PinB)
-        ret = (a_state ^ b_state) | b_state
-        return ret
+        self.gpio = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
 
+        self.gpio.pinMode(self.a_pin, self.gpio.INPUT)
+        self.gpio.pullUpDnControl(self.a_pin, self.gpio.PUD_UP)
+
+        self.gpio.pinMode(self.b_pin, self.gpio.INPUT)
+        self.gpio.pullUpDnControl(self.b_pin, self.gpio.PUD_UP)
+
+        self.last_delta = 0
+        self.r_seq = self.rotation_sequence()
+
+    # Gets the 2-bit rotation state of the current position
+    # This is deprecated - we now use rotation_sequence instead.
+    def rotation_state(self):
+        a_state = self.gpio.digitalRead(self.a_pin)
+        b_state = self.gpio.digitalRead(self.b_pin)
+        r_state = a_state | b_state << 1
+        return r_state
+
+    # Returns the quadrature encoder state converted into
+    # a numerical sequence 0,1,2,3,0,1,2,3...
+    #    
+    # Turning the encoder clockwise generates these
+    # values for switches B and A:
+    #  B A
+    #  0 0
+    #  0 1
+    #  1 1
+    #  1 0 
+    # We convert these to an ordinal sequence number by returning
+    #   seq = (A ^ B) | B << 2
+    # 
+    def rotation_sequence(self):
+        a_state = self.gpio.digitalRead(self.a_pin)
+        b_state = self.gpio.digitalRead(self.b_pin)
+        r_seq = (a_state ^ b_state) | b_state << 1
+        return r_seq
+
+    # Returns offset values of -2,-1,0,1,2
     def get_delta(self):
         delta = 0
-        r_r = self.read_rot()
-        if r_r != self.r_r:
-            delta = (r_r - self.r_r)
-            # if delta==3:
-            #     delta = -1
-            # elif delta==2:
-            #     delta = int(math.copysign(delta, self.last_delta))  # same direction as previous, 2 steps
+        r_seq = self.rotation_sequence()
+        if r_seq != self.r_seq:
+            delta = (r_seq - self.r_seq) % 4
+            if delta==3:
+                delta = -1
+            elif delta==2:
+                delta = int(math.copysign(delta, self.last_delta))  # same direction as previous, 2 steps
                 
             self.last_delta = delta
-            self.r_r = r_r
+            self.r_seq = r_seq
 
         return delta
 
-        
-if __name__ == '__main__':
-    my_encoder = RotaryEnc(5, 7, 3)
-    my_encoder.bin_worker()
+    class Worker(threading.Thread):
+        def __init__(self, a_pin, b_pin):
+            threading.Thread.__init__(self)
+            self.lock = threading.Lock()
+            self.encoder = RotaryEncoder(a_pin, b_pin)
+            self.daemon = True
+            self.delta = 0
+
+        def run(self):
+            while True:
+                delta = self.encoder.get_delta()
+                with self.lock:
+                    self.delta += delta
+                time.sleep(0.001)
+
+        def get_delta(self):
+            # revisit - should use locking
+            with self.lock:
+                delta = self.delta
+                self.delta = 0
+            return delta
