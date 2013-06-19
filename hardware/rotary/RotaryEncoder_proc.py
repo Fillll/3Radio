bac4b398215eea25269e96bfafd2da033faeaef2
Fillll <spam@fillll.ru>
2013-06-19 11:49:15 +0400

    RE: работа процесса на экодере (вслепую)
    Писал вслепую.

--- hardware/rotary/RotaryEncoder_proc.py
+++ hardware/rotary/RotaryEncoder_proc.py
@@ -7,7 +7,34 @@ from multiprocessing import Process
 
 
 class REException(Exception):
-	pass
+    pass
 
 
-# ...
+def RE_runner(q):
+    my_RE = RotaryEncoder.RotaryEncoder(7, 9)
+
+    p = Process(target=RE_worker, args=(1, my_RE))
+    p.start()
+
+def RE_worker(q, encoder):
+    total_delta = 0
+    while True:
+        delta = encoder.delta() # returns 0,1,or -1
+        if delta!=0:
+            total_delta += delta
+            if (total_delta % 4 == 0) and (total_delta != 0):
+                q.put({'rot':total_delta/4})
+                total_delta = 0
+            interval = 0.001
+            nothing_happens = 0
+        else:
+            nothing_happens += 1
+
+        # sleeping
+        if nothing_happens == 1000:
+            interval = 0.01
+        elif nothing_happens == 100000:
+            interval = 0.1
+        elif nothing_happens == 10000000:
+            interval = 1
+        time.sleep(interval)
